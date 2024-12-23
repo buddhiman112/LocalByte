74a8f5c1687fdf2f15d7b5615954f1e251b66d21
buddhiman bk <buddhiman.bk23@midvalley.edu.np>
2024-12-23 23:14:36 +0545

    some changes into main file

--- script.js
+++ script.js
@@ -111,6 +111,54 @@ navMenu.addEventListener('click', (event) => {
 });
 
 
+// JavaScript to toggle the visibility of the menu
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+// Image Slider functionality
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Automatically show the first slide
+showSlides(slideIndex);
 
 
 
